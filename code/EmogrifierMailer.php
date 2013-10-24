aa5a989f67f8f07ed6a6550c1bdaa302f22c11ed
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-10-24 22:20:41 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/newsletter_emogrify/branches/2.4@5374 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/EmogrifierMailer.php
+++ code/EmogrifierMailer.php
@@ -6,13 +6,18 @@ class EmogrifierMailer extends Mailer {
 	public static function set_css_file($s){ self::$css_file = $s;}
 
 	function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false, $inlineImages = false) {
-		require_once(Director::baseFolder() . '/newsletter_emogrify/thirdparty/Emogrifier.php');
-		$cssFileLocation = Director::baseFolder() . self::$css_file;
-		$cssFileHandler = fopen($cssFileLocation, 'r');
-		$css = fread($cssFileHandler,  filesize($cssFileLocation));
-		fclose($cssFileHandler);
-		$emog = new Emogrifier($htmlContent, $css);
-		$htmlContent = $emog->emogrify();
+		if(strpos('/Security/changepassword')) {
+			//do nothing, because it crashes
+		}
+		else {
+			require_once(Director::baseFolder() . '/newsletter_emogrify/thirdparty/Emogrifier.php');
+			$cssFileLocation = Director::baseFolder() . self::$css_file;
+			$cssFileHandler = fopen($cssFileLocation, 'r');
+			$css = fread($cssFileHandler,  filesize($cssFileLocation));
+			fclose($cssFileHandler);
+			$emog = new Emogrifier($htmlContent, $css);
+			$htmlContent = $emog->emogrify();
+		}
 		return parent::sendHTML($to, $from, $subject, $htmlContent, $attachedFiles, $customheaders, $plainContent, $inlineImages);
 	}
 }
